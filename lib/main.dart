036abf7effaa1055505cad775e25d447bfbff6e3
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-05-17 11:44:57 +0530

    Used backgroundColor with Color.fromARGB to add background

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 59, 45, 85),
         body: Center(
           child: Text("Hello World"),
         ),
